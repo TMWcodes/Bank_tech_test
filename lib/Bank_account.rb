fc9c2ad58ba4e5daafba48155dc58e5ceb346b32
Tyrone Williams <Tyronemwilliams@outlook.com>
2022-02-21 22:14:26 +0000

    withdraw and deposit

--- lib/Bank_account.rb
+++ lib/Bank_account.rb
@@ -9,8 +9,19 @@ class BankAccount
    def initialize(checking_deposit)
       @checking = checking_deposit
    end
+   
+   def deposit(amount)
+      @checking  += amount
+   end
 
+   def withdraw(amount)
+      if amount > @checking
+         p "inadequate funds"
+      else @checking -= amount
+      end
+   end
 end
 
 account = BankAccount.new(600)
-p account
+p account.deposit(300)
+p account.withdraw(300)
--- spec/Bank_account_spec.rb
+++ spec/Bank_account_spec.rb
@@ -3,9 +3,9 @@
 require 'Bank_account'
 
 describe BankAccount do
-   before do
-    account = BankAccount.new(600)
-   end
+#    before do
+#     account = BankAccount.new(600)
+#    end
 
   it 'exists' do
     expect(BankAccount.new(600)).not_to be_nil
@@ -15,5 +15,17 @@ describe BankAccount do
     account = BankAccount.new(600)
     expect(account.checking).to eq(600)
   end
+
+  it'can receive monies' do
+    account = BankAccount.new(600)
+    expect(account.deposit(300)).to eq(900)
+  end 
+
+  it'can dispense money' do
+    account = BankAccount.new(600)
+    expect(account.withdraw(300)).to eq(300)
+  end
+
+
 
 end
