2a6fe4c8f7c38be8adac72c91087c1ed22b654e4
Tyrone Williams <Tyronemwilliams@outlook.com>
2022-02-22 18:58:11 +0000

    functional version, pre-refactor

--- lib/Bank_account.rb
+++ lib/Bank_account.rb
@@ -8,13 +8,15 @@ class BankAccount
    public
    def initialize(initial_deposit)
       @balance = initial_deposit
-      @transactions = []
+      @debit_transactions = []
+      @credit_transactions = []
       @date = Time.new.strftime("%d/%m/%Y")
+      @transactions = @debit_transactions+@credit_transactions
    end
  
    def deposit(amount)
       @balance  += amount
-      @transactions<<[@date,amount, @balance]
+      @transactions<<[@date,amount,0, @balance]
       @balance
    end
 
@@ -23,38 +25,31 @@ class BankAccount
          p "inadequate funds"
       else 
          @balance -= amount
-         @transactions<<[@date,amount, @balance]
+         @transactions<<[@date,0,amount, @balance]
          @balance
       end
    end
-
-   def transactions
-       @transactions
-   end
+   
 
    def account_info
-      "Balance is #{@balance}," + " with #{@transactions.count} transactions so far"
-   end
-
+      "Balance is #{@balance}," + " with #{(@transactions).count} transactions so far"
+   end 
 
    def display
-      "date       || credit || debit || balance\n"
-      puts  "date       || transactons || balance\n"
-      @transactions.each do |tr|
+      puts "date       || credit || debit || balance\n"
+       "date       || transactons || balance\n"
+      @transactions.reverse.each do |tr|
       puts tr.join(" || ") 
       end
    end
 end
 
-account = BankAccount.new(600)
+p account = BankAccount.new(600)
 p "opened an account with £600" 
 p "deposited money leaving a new balance of #{account.deposit(200)}"
 p "withdrew money leaving a balance of #{account.withdraw(350)}"
+p "withdrew money leaving a balance of #{account.withdraw(250)}"
 p account.account_info
-print account.display
-# p account.transactions
+p"-----"
+p account.display
 p "------"
-# p DateTime.now
-# p Date.today
-
-# p Time.new.strftime("%d/%m/%Y")
